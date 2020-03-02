7f76a4431548a8d578bf0ea1943ffec11d08d512
Daniele Cesarini <d.cesarini@cineca.it>
2020-03-03 00:14:52 +0100

    Removed hwloc vendoring

--- cmake/FindHWLOC.cmake
+++ cmake/FindHWLOC.cmake
@@ -1,44 +1,92 @@
 # First check for user-specified HWLOC_DIR
 if(HWLOC_DIR)
-    MESSAGE(STATUS "Looking for Hwloc using HWLOC_DIR = ${HWLOC_DIR}")
+    set(HWLOC_INCLUDE_DIR ${HWLOC_DIR}/include)
+    set(HWLOC_LIBRARIES_DIR ${HWLOC_DIR}/lib)
 
-    set(HWLOC_FOUND TRUE)
-    set(HWLOC_INCLUDE_DIRS ${HWLOC_DIR}/include)
-    set(HWLOC_LIBRARY ${HWLOC_DIR}/lib/libhwloc.so)
+    find_library(HWLOC_LIBRARIES
+        NAMES hwloc
+        PATHS ${HWLOC_INCLUDE_DIR}
+    )
 
-    set(HWLOC_DIR ${HWLOC_DIR} CACHE PATH "" FORCE)
+    find_path(HWLOC_INCLUDE_DIRS
+        NAMES hwloc.h
+        PATHS ${HWLOC_LIBRARIES_DIR}
+    )
 
-    message(STATUS "FOUND Hwloc at ${HWLOC_DIR}")
-    message(STATUS "  HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
-    message(STATUS "  HWLOC_LIBRARY = ${HWLOC_LIBRARY}")
+    if(HWLOC_INCLUDE_DIRS AND HWLOC_LIBRARIES)
+        set(HWLOC_FOUND TRUE)
+        if(NOT HWLOC_FIND_QUIETLY)
+            message(STATUS "Found HWLOC using HWLOC_DIR = ${HWLOC_DIR}")
+            message(STATUS "  HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
+            message(STATUS "  HWLOC_LIBRARIES = ${HWLOC_LIBRARIES}")
+        endif()
+    else()
+        set(HWLOC_FOUND FALSE)
+    endif()
 # If HWLOC_DIR not specified, then try to automatically find the HWLOC header
 # and library
 elseif(NOT HWLOC_DIR)
-    find_path(HWLOC_INCLUDE_DIRS
-        NAMES hwloc.h
-    )
+    # Try to find HWLOC using pkg-config
+    find_package(PkgConfig QUIET)
+    if(PkgConfig_FOUND)
+         pkg_check_modules(HWLOC hwloc)
+    endif()
 
-    find_library(HWLOC_LIBRARY
-        NAMES libhwloc.so
-    )
+    if(HWLOC_FOUND)
+        set(HWLOC_LIBRARIES ${HWLOC_LDFLAGS})
+        if(NOT HWLOC_FIND_QUIETLY)
+            message(STATUS "Found HWLOC using pkg-config")
+            message(STATUS "  HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
+            message(STATUS "  HWLOC_LIBRARIES = ${HWLOC_LIBRARIES}")
+        endif()
+    else()
+        find_library(HWLOC_LIBRARIES
+            NAMES hwloc
+            PATHS ENV LD_LIBRARY_PATH
+        )
 
-    if(HWLOC_INCLUDE_DIRS AND HWLOC_LIBRARY)
-        set(HWLOC_FOUND TRUE)
-        message(STATUS "HWLOC library found using find_library()")
-        message(STATUS "  HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
-        message(STATUS "  HWLOC_LIBRARY = ${HWLOC_LIBRARY}")
+        set(_extralibdirs)
+        if(DEFINED $ENV{HWLOC_HOME})
+            list(APPEND _extralibdirs $ENV{HWLOC_HOME}/include)
+        endif()
+        if(DEFINED $ENV{HWLOC_ROOT})
+            list(APPEND _extralibdirs $ENV{HWLOC_ROOT}/include)
+        endif()
+        if(DEFINED $ENV{HWLOC_PATH})
+            list(APPEND _extralibdirs $ENV{HWLOC_PATH}/include)
+        endif()
+        if(DEFINED $ENV{HWLOC_DIR})
+            list(APPEND _extralibdirs $ENV{HWLOC_DIR}/include)
+        endif()
+        if(HWLOC_LIBRARIES)
+            get_filename_component(_hwloc_lib "${HWLOC_LIBRARIES}" PATH)
+            get_filename_component(_hwloc "${_hwloc_lib}" PATH)
+            list(APPEND _extralibdirs ${_hwloc}/include)
+        endif()
+
+        find_path(HWLOC_INCLUDE_DIRS
+            NAMES hwloc.h
+            PATHS _extralibdirs
+        )
+
+        if(HWLOC_INCLUDE_DIRS AND HWLOC_LIBRARIES)
+            set(HWLOC_FOUND TRUE)
+            if(NOT HWLOC_FIND_QUIETLY)
+                message(STATUS "Found HWLOC library using find_library()")
+                message(STATUS "  HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
+                message(STATUS "  HWLOC_LIBRARIES = ${HWLOC_LIBRARIES}")
+            endif()
+        endif()
     endif()
 endif()
 
-# If HWLOC is still not found, then download and build HWLOC from source
-if(NOT HWLOC_INCLUDE_DIRS AND NOT HWLOC_LIBRARY)
-    MESSAGE(STATUS "Downloading and building Hwloc from source")
-    include(CMake/SetupExternalProjects.cmake)
-
-    set(HWLOC_EXT TRUE)
-
-    message(STATUS "ExternalProject Hwloc")
-    message(STATUS "  HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
-    message(STATUS "  HWLOC_LIBRARY = ${HWLOC_LIBRARY}")
-    set(HWLOC_FOUND TRUE)
+# If HWLOC is still not found
+if(NOT HWLOC_FOUND)
+    if(HWLOC_FIND_REQUIRED)
+        message(FATAL_ERROR "HWLOC not Found!")
+    else()
+        if(NOT HWLOC_FIND_QUIETLY)
+            message(WARNING "HWLOC not Found!")
+        endif()
+    endif()
 endif()
